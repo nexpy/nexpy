22efc006c4af66e9a6ab354a32b67b8fd6719671
Ray Osborn <rayosborn@mac.com>
2013-06-18 18:26:08 -0500

    Added a new plot dialog
    Designed to allow arbitrary NXfields to be plotted against each other.
    Still needs debugging.

--- src/gui/datadialogs.py
+++ src/gui/datadialogs.py
@@ -1,18 +1,60 @@
 # System library imports
 from IPython.external.qt import QtGui,QtCore
 
-"""
-Dialog for renaming a NeXus node
-"""
-class RenameDialog(QtGui.QDialog):
-    """Dialog to select a text file"""
+# NeXpy imports
+from nexpy.api.nexus import NXfield, NXgroup, NXdata
+
+class PlotDialog(QtGui.QDialog):
+    """Dialog to plot arbitrary NeXus data"""
  
     def __init__(self, node, parent=None):
 
         QtGui.QDialog.__init__(self, parent)
  
         self.node = node
-        self.view = parent
+        
+        if isinstance(node, NXfield):
+            plotlayout = QtGui.QHBoxLayout()
+            self.plotbox = QtGui.QComboBox()
+            for entry in self.node.nxgroup.entries:
+                self.plotbox.addItem(node.nxgroup[entry].nxname)
+            plotlabel = QtGui.QLabel("Choose x-axis: ")
+            plotlayout.addWidget(plotlabel)
+            plotlayout.addWidget(self.plotbox)
+
+        buttonbox = QtGui.QDialogButtonBox(self)
+        buttonbox.setOrientation(QtCore.Qt.Horizontal)
+        buttonbox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        buttonbox.accepted.connect(self.accept)
+        buttonbox.rejected.connect(self.reject)
+
+        layout = QtGui.QVBoxLayout()
+        layout.addLayout(plotlayout)
+        layout.addWidget(buttonbox) 
+        self.setLayout(layout)
+
+        self.setWindowTitle("Plot NeXus Field")
+
+    def get_axis(self):
+        return self.plotbox.itemData(self.plotbox.currentIndex())
+	
+    def accept(self):
+        data = NXdata(self.node, self.node.nxgroup[self.get_axis()])
+        data.plot()
+        QtGui.QDialog.accept(self)
+        
+    def reject(self):
+        QtGui.QDialog.reject(self)
+
+    
+class RenameDialog(QtGui.QDialog):
+    """Dialog to rename a NeXus node"""
+ 
+    def __init__(self, node, parent=None):
+
+        QtGui.QDialog.__init__(self, parent)
+ 
+        self.node = node
  
         namelayout = QtGui.QHBoxLayout()
         label = QtGui.QLabel("New Name: ")
