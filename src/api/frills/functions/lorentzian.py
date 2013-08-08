76e4e47644c2bf8ea4e8f486e4be97844a387e8e
Ray Osborn <rayosborn@mac.com>
2013-08-08 16:59:47 -0500

    New Lorentzian function

---
+++ src/api/frills/functions/lorentzian.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+function_name = 'Lorentzian'
+parameters = ['Integral', 'Gamma', 'Center']
+
+def values(x, p):
+    integral, gamma, center = p
+    return integral * (gamma/np.pi) / ((x-center)**2 + gamma**2)
+
+def guess(x, y):
+    center = (x*y).sum()/y.sum()
+    gamma = np.sqrt(((x-center)**2*y).sum()/y.sum())
+    integral = y.max() * np.pi * gamma
+    return integral, gamma, center
