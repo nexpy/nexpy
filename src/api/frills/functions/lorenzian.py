0eb77e1a7ec28531445b5f553c25b393cca86e87
Ray Osborn <rayosborn@mac.com>
2013-08-08 16:58:31 -0500

    Renamed Lorentzian function to follow usual spelling
    Not sure the usual spelling is correct but it's much more common.

--- src/api/frills/functions/lorenzian.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-function_name = 'Lorenzian'
-parameters = ['Integral', 'Gamma', 'Center']
-
-def values(x, p):
-    integral, gamma, center = p
-    return integral * (gamma/np.pi) / ((x-center)**2 + gamma**2)
