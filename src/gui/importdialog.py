78119ceb8609ce4b726c67d81e99899864dccb10
Ray Osborn <rayosborn@mac.com>
2013-08-24 22:43:28 -0500

    Added docstrings to document data imports

--- src/gui/importdialog.py
+++ src/gui/importdialog.py
@@ -6,10 +6,10 @@ from IPython.external.qt import QtCore, QtGui
 
 from nexpy.api.nexus import *
 
-filetype = "Text File"
+filetype = "Text File" #Defines the Import Menu label
 
 class BaseImportDialog(QtGui.QDialog):
-    """Dialog to select a text file"""
+    """Base dialog class for NeXpy import dialogs"""
  
     def __init__(self, parent=None):
 
@@ -17,6 +17,9 @@ class BaseImportDialog(QtGui.QDialog):
         self.accepted = False 
 
     def filebox(self):
+        """
+        Creates a text box and button for selecting a file.
+        """
         self.filebutton =  QtGui.QPushButton("Choose File")
         self.filebutton.clicked.connect(self.choose_file)
         self.filename = QtGui.QLineEdit(self)
@@ -27,6 +30,9 @@ class BaseImportDialog(QtGui.QDialog):
         return filebox
  
     def directorybox(self):
+        """
+        Creates a text box and button for selecting a directory.
+        """
         self.directorybutton =  QtGui.QPushButton("Choose Directory")
         self.directorybutton.clicked.connect(self.choose_directory)
         self.directoryname = QtGui.QLineEdit(self)
@@ -37,6 +43,9 @@ class BaseImportDialog(QtGui.QDialog):
         return directorybox
 
     def buttonbox(self):
+        """
+        Creates a box containing the standard Cancel and OK buttons.
+        """
         buttonbox = QtGui.QDialogButtonBox(self)
         buttonbox.setOrientation(QtCore.Qt.Horizontal)
         buttonbox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|
@@ -47,13 +56,16 @@ class BaseImportDialog(QtGui.QDialog):
 
     def choose_file(self):
         """
-        Opens a file dialog and sets the file text box to the chosen path
+        Opens a file dialog and sets the file text box to the chosen path.
         """
         filename, _ = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
             os.path.expanduser('~'))
         self.filename.setText(str(filename))
 
     def get_filename(self):
+        """
+        Returns the selected file.
+        """
         return self.filename.text()
 
     def choose_directory(self):
@@ -65,20 +77,36 @@ class BaseImportDialog(QtGui.QDialog):
         self.directoryname.setText(str(dir))
 
     def get_directory(self):
+        """
+        Returns the selected directory
+        """
         return self.directoryname.text()
 
     def get_filesindirectory(self):
+        """
+        Returns a list of files in the selected directory.
+        
+        The files are sorted using a natural sort algorithm that preserves the
+        numeric order when a file name consists of text and index so that, e.g., 
+        'data2.tif' comes before 'data10.tif'.
+        """
         os.chdir(self.get_directory())
         filenames = os.listdir(os.getcwd())
         return sorted(filenames,key=natural_sort)
 
     def accept(self):
+        """
+        Completes the data import.
+        """
         self.accepted = True
         from nexpy.gui.consoleapp import _mainwindow
         _mainwindow.import_data()
         QtGui.QDialog.accept(self)
         
     def reject(self):
+        """
+        Cancels the data import.
+        """
         self.accepted = False
         QtGui.QDialog.reject(self)
 
