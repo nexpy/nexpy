15dd8f8132168b91d4d779d59e60bc885868ca46
Ray Osborn <rayosborn@mac.com>
2014-04-15 10:31:16 -0500

    Replacing wild card import of nexpy.api.nexus with explicit imports

--- src/nexpy/readers/readstack.py
+++ src/nexpy/readers/readstack.py
@@ -28,7 +28,7 @@ Two GUI elements are provided for convenience:
 from IPython.external.qt import QtGui, QtCore
 import os, re
 import numpy as np
-from nexpy.api.nexus import *
+from nexpy.api.nexus import NXfield, NXentry, NXdata
 from nexpy.gui.importdialog import BaseImportDialog
 
 filetype = "Image Stack"
