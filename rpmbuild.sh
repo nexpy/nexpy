b93c66fb0e97d72e71daf20dfb6a764e729465d3
Pete Peterson <petersonpf@ornl.gov>
2015-02-23 09:13:54 -0500

    Bash script for building an rpm

---
+++ rpmbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+./setup.py bdist_rpm --requires="h5py,numpy >= 1.6.0,scipy,python-ipython >= 1.1.0,python-matplotlib >= 1.2.0,python-pyside >= 1.1.0"
+#################################################################################
+# python-pyside v1.1.0 rpm has a bug in it where it does not supply the egg-info
+# that dist-utils looks for. There is a workaround mentioned in
+# https://github.com/nvbn/everpad/issues/401#issuecomment-35834335 which is to
+# spoof the system with a fake file in
+# /usr/lib/python2.7/dist-packages/PySide-1.1.0-py2.7.egg-info
+# or
+# /usr/lib/python2.7/site-packages/PySide-1.1.0-py2.7.egg-info
+# depending on which install location exists. It appears that v1.2.0 contains
+# the missing file.
+#################################################################################
