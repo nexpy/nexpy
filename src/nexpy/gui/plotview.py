8e707b1a6c492536f67ff1743dcc5a056c43f30b
Ray Osborn <rayosborn@mac.com>
2015-06-05 13:45:08 -0500

    Fixed problem with cursor coordinate display
    Did not work with reversed axes.

--- src/nexpy/gui/plotview.py
+++ src/nexpy/gui/plotview.py
@@ -997,8 +997,14 @@ class NXPlotView(QtGui.QWidget):
 
     def format_coord(self, x, y):
         try:
-            col = np.searchsorted(plotview.xaxis.boundaries-x, 0.0) - 1
-            row = np.searchsorted(plotview.yaxis.boundaries-y, 0.0) - 1
+            if plotview.xaxis.reversed:
+                col = np.searchsorted(x-plotview.xaxis.boundaries, 0.0) - 1
+            else:
+                col = np.searchsorted(plotview.xaxis.boundaries-x, 0.0) - 1
+            if plotview.yaxis.reversed:
+                row = np.searchsorted(y-plotview.yaxis.boundaries, 0.0) - 1
+            else:
+                row = np.searchsorted(plotview.yaxis.boundaries-y, 0.0) - 1
             z = self.v[row,col]
             return 'x=%1.4f y=%1.4f\nv=%1.4g'%(x, y, z)
         except:
