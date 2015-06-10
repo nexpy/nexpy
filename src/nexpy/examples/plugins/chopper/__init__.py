81ac9096f8938c4fb691e2c8bb441fd8f4c21463
Ray Osborn <rayosborn@mac.com>
2015-06-09 19:26:45 -0500

    More PyQt compatibility changes
    Adding QMenu items to the main menu bar does not work the same way as
    in PySide, so the only things defined in the plugin are now the menu
    text and the triggered functions.

--- src/nexpy/examples/plugins/chopper/__init__.py
+++ src/nexpy/examples/plugins/chopper/__init__.py
@@ -1,10 +1,8 @@
-from matplotlib.backends.qt_compat import QtGui
 import get_ei, convert_qe
 
-def plugin_menu(parent):
-    menu = QtGui.QMenu('Chopper')
-    menu.addAction(QtGui.QAction('Get Incident Energy', parent, 
-                   triggered=get_ei.show_dialog))
-    menu.addAction(QtGui.QAction('Convert to Q-E', parent, 
-                   triggered=convert_qe.show_dialog))
-    return menu
+def plugin_menu():
+    menu = 'Chopper'
+    actions = []
+    actions.append(('Get Incident Energy', get_ei.show_dialog))
+    actions.append(('Convert to Q-E', convert_qe.show_dialog))
+    return menu, actions
