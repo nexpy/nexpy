2b1b077c55f35539adfd88b58abdf1e53458dd10
Ray Osborn <rayosborn@mac.com>
2018-02-03 13:41:33 -0600

    Convert CBF and image stack imports to use Fabio
    It is difficult to install pycbf, whereas the ESRF Fabio package is available on PyPI and Conda Forge. It will also read many other formats, such as Mar and GE detector files and the ESRF EDF format.

--- src/nexpy/readers/readcbf.py
+++ src/nexpy/readers/readcbf.py
@@ -16,7 +16,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
-import pycbf
 
 from nexusformat.nexus import *
 from nexpy.gui.importdialog import BaseImportDialog
@@ -35,19 +34,19 @@ class ImportDialog(BaseImportDialog):
         self.setWindowTitle("Import "+str(filetype))
  
     def get_data(self):
+        try:
+            import fabio
+        except ImportError:
+            raise NeXusError("Please install the 'fabio' module")
         self.import_file = self.get_filename()
-        cbf = pycbf.cbf_handle_struct()
-        cbf.read_file(str(self.import_file), pycbf.MSG_DIGEST)
-        cbf.select_datablock(0)
-        cbf.select_category(0)
-        cbf.select_column(2)
-        imsize = cbf.get_image_size(0)
-        z = NXfield(np.fromstring(cbf.get_integerarray_as_string(),np.int32).reshape(imsize), name='z')
+        im = fabio.open(self.import_file)
+        z = NXfield(im.data, name='z')
         y = NXfield(range(z.shape[0]), name='y')
-        x = NXfield(range(z.shape[1]), name='x')
-        
-        cbf.select_column(1)
-        notes = NXnote(type='text/plain', description='CBF Header', 
-                       data=cbf.get_value().replace('\n','\r\n'))
-        
-        return NXentry(NXdata(z,(y,x)), CBF_header=notes)
+        x = NXfield(range(z.shape[1]), name='x')        
+        note = NXnote(type='text/plain', file_name=self.import_file)
+        note.data = im.header.pop('_array_data.header_contents', '')
+        note.description = im.header.pop('_array_data.header_convention', '')
+        header = NXcollection()
+        for k, v in im.header.items():
+            header[k] = v
+        return NXentry(NXdata(z,(y,x), CBF_header=note, header=header))
