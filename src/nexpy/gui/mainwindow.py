1f33690e0cd7a085b2f08a969b942fd97c5a1bfd
Ray Osborn <rayosborn@mac.com>
2018-05-11 12:43:33 -0500

    Add QApplication.closeAllWindows to the shutdown

--- src/nexpy/gui/mainwindow.py
+++ src/nexpy/gui/mainwindow.py
@@ -2191,6 +2191,7 @@ class MainWindow(QtWidgets.QMainWindow):
         if confirm_action("Are you sure you want to quit NeXpy?", 
                           icon=self.app.icon_pixmap):
             logging.info('NeXpy closed\n'+80*'-')
+            self._app.closeAllWindows()
             self._app.quit()
             return event.accept()
         else:
