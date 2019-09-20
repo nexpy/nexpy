a71f79527165bfc07a4175e163fa62905f8ff544
Ray Osborn <rayosborn@mac.com>
2019-09-20 15:30:27 -0500

    Add basic import tests

---
+++ tests/test_imports.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+def test_api_imports():
+    try:
+        import nexpy.api.frills.fit
+    except ImportError as error:
+        pytest.fail(str(error))
+
+
+def test_gui_import():
+    try:
+        import nexpy.gui.consoleapp
+        import nexpy.gui.datadialogs
+        import nexpy.gui.fitdialogs
+        import nexpy.gui.importdialog
+        import nexpy.gui.mainwindow
+        import nexpy.gui.plotview
+        import nexpy.gui.pyqt
+        import nexpy.gui.scripteditor
+        import nexpy.gui.treeview
+        import nexpy.gui.utils
+        import nexpy.gui.widgets
+    except ImportError as error:
+        pytest.fail(str(error))
+
+
+def test_reader_import():
+    try:
+        import nexpy.readers.readspe
+        import nexpy.readers.readspec
+        import nexpy.readers.readstack
+        import nexpy.readers.readtiff
+        import nexpy.readers.readtxt
+    except ImportError as error:
+        pytest.fail(str(error))
