cadac23137f0801b10a63e50b0b1568be5c8bd18
Ray Osborn <rayosborn@mac.com>
2020-05-22 09:20:54 -0500

    Improve docstrings for NeXpy models

--- src/nexpy/api/frills/models/pdfdecay.py
+++ src/nexpy/api/frills/models/pdfdecay.py
@@ -3,12 +3,20 @@ import numpy as np
 from lmfit.model import Model
 
 class PDFdecayModel(Model):
-    """A model to describe the product of a decaying exponential and a Gaussian
-    with three parameters: ``amplitude``, ``xi``, and ``sigma``.
+    r"""A model to describe the product of a decaying exponential and a Gaussian
+    with three parameters: ``amplitude``, ``xi``, and ``sigma``
+
+    .. math::
+
+        f(x; A, \xi, \sigma) = A e^{[-{|x|}/\xi]} e^{[{-{x^2}/{{2\sigma}^2}}]}
+
+    where the parameter ``amplitude`` corresponds to :math:`A`, ``xi`` to 
+    :math:`\xi`, and ``sigma`` to :math:`\sigma`. 
+
     """
     def __init__(self, **kwargs):
 
-        def pdfdecay(x, amplitude=1.0, xi=1.9, sigma=1.0):
+        def pdfdecay(x, amplitude=1.0, xi=1.0, sigma=1.0):
             return amplitude * np.exp(-abs(x)/xi) * np.exp(-x**2/(2*sigma**2))
 
         super().__init__(pdfdecay, **kwargs)
