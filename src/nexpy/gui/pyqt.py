8df5a958f27194088131df6da82507991680360d
Ray Osborn <rayosborn@mac.com>
2021-04-27 12:20:11 -0500

    Update copyright dates

--- src/nexpy/gui/pyqt.py
+++ src/nexpy/gui/pyqt.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013-2020, NeXpy Development Team.
+# Copyright (c) 2013-2021, NeXpy Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
