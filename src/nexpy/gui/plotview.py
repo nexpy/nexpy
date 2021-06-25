1e23b16616054066142ed79d01ae301e92f89bba
Ray Osborn <rayosborn@mac.com>
2021-06-24 21:51:07 -0500

    Transfer grid options to MPL plot

--- src/nexpy/gui/plotview.py
+++ src/nexpy/gui/plotview.py
@@ -1869,7 +1869,7 @@ class NXPlotView(QtWidgets.QDialog):
         self._nameonly = False
         self.draw()
 
-    def grid(self, display=None, minor=False, **opts):
+    def grid(self, display=None, minor=False, ax=None, **opts):
         """Set grid display.
         
         Parameters
@@ -1883,6 +1883,8 @@ class NXPlotView(QtWidgets.QDialog):
             Valid options for displaying grids. If not set, the default
             Matplotlib styles are used.
         """
+        if ax is None:
+            ax = self.ax
         if display is not None:
             self._grid = display
         elif opts:
@@ -1904,17 +1906,17 @@ class NXPlotView(QtWidgets.QDialog):
             else:
                 opts['color'] = self._gridcolor
             if minor:
-                self.ax.minorticks_on()
+                ax.minorticks_on()
             if self.skew:
                 self.draw_skewed_grid(minor=minor, **opts)
             else:
-                self.ax.grid(True, which='major', axis='both', **opts)
+                ax.grid(True, which='major', axis='both', **opts)
                 if minor:
                     opts['linewidth'] = max(self._gridwidth/2, 0.1)
-                    self.ax.grid(True, which='minor', axis='both', **opts)
+                    ax.grid(True, which='minor', axis='both', **opts)
                 self.remove_skewed_grid()
         else:
-            self.ax.grid(False, which='both', axis='both')
+            ax.grid(False, which='both', axis='both')
             if not self._minorticks:
                 self.minorticks_off()
             if self.skew:
@@ -2394,6 +2396,7 @@ class NXPlotView(QtWidgets.QDialog):
             ax.text(0.02, 0.95, plot_label, fontsize=16,
             horizontalalignment='left', verticalalignment='top', color='black',
             transform=ax.transAxes)
+        self.grid(display=self._grid, minor=self._minorgrid, ax=ax)
 
     def block_signals(self, block=True):
         self.xtab.block_signals(block)
