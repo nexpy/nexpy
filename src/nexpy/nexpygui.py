d4490c3a2a4531fbc76bd9e31f6a165fcdb46c2f
Ray Osborn <rayosborn@mac.com>
2021-12-01 09:00:26 -0600

    Codestyle corrections

--- src/nexpy/nexpygui.py
+++ src/nexpy/nexpygui.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (c) 2013-2021, NeXpy Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING, distributed with this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 import argparse
 import sys
+
 import nexpy
 
 
@@ -17,13 +18,13 @@ def main():
 
     parser = argparse.ArgumentParser(description="Launch NeXpy")
 
-    parser.add_argument('filenames', nargs='*', 
+    parser.add_argument('filenames', nargs='*',
                         help='NeXus file(s) to open on launch (optional)')
-    parser.add_argument('-v', '--version', action='version', 
+    parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s v'+nexpy.__version__)
-    parser.add_argument('-r', '--restore', action='store_true', 
+    parser.add_argument('-r', '--restore', action='store_true',
                         help='open files from previous session')
-    parser.add_argument('-f', '--faulthandler', action='store_true', 
+    parser.add_argument('-f', '--faulthandler', action='store_true',
                         help='enable faulthandler for system crashes')
     args, extra_args = parser.parse_known_args()
 
