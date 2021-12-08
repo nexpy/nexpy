45e7700a0512c36bd673aed9503680ca90808d48
Ray Osborn <rayosborn@mac.com>
2021-12-08 14:14:34 -0600

    Remove unnecessary utf-8 coding statements

--- src/nexpy/__init__.py
+++ src/nexpy/__init__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # -----------------------------------------------------------------------------
 # Copyright (c) 2013-2021, NeXpy Development Team.
 #
--- src/nexpy/nexpygui.py
+++ src/nexpy/nexpygui.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # -----------------------------------------------------------------------------
 # Copyright (c) 2013-2021, NeXpy Development Team.
 #
