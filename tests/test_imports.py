89ceecd6f0daa24744fc2bb9ce6009da4f40bb8a
Richard Osborn <richardosborn@mac.com>
2026-01-11 15:31:41 -0600

    test: add multi-binding CI matrix with uv
    - Test PyQt5 and PyQt6 across Python 3.10-3.14 on Linux
    - Spot-check PySide6 on Linux
    - Spot-check PyQt5/PyQt6 on macOS and Windows
    - Remove hardcoded QT_API from tests, let qtpy auto-detect

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,33 +1,25 @@
-import os
-
-import pytest
+"""Import tests to verify all modules can be loaded."""
 
 
 def test_gui_import():
-    os.environ['QT_API'] = 'pyqt5'
-    try:
-        import nexpy.gui.pyqt
-        import nexpy.gui.consoleapp
-        import nexpy.gui.dialogs
-        import nexpy.gui.fitdialogs
-        import nexpy.gui.importdialog
-        import nexpy.gui.mainwindow
-        import nexpy.gui.plotview
-        import nexpy.gui.scripteditor
-        import nexpy.gui.treeview
-        import nexpy.gui.utils
-        import nexpy.gui.widgets
-    except ImportError as error:
-        pytest.fail(str(error))
+    """Test that all GUI modules can be imported."""
+    import nexpy.gui.pyqt
+    import nexpy.gui.consoleapp
+    import nexpy.gui.dialogs
+    import nexpy.gui.fitdialogs
+    import nexpy.gui.importdialog
+    import nexpy.gui.mainwindow
+    import nexpy.gui.plotview
+    import nexpy.gui.scripteditor
+    import nexpy.gui.treeview
+    import nexpy.gui.utils
+    import nexpy.gui.widgets
 
 
 def test_reader_import():
-    os.environ['QT_API'] = 'pyqt5'
-    try:
-        import nexpy.gui.pyqt
-        import nexpy.readers.readspec
-        import nexpy.readers.readstack
-        import nexpy.readers.readtiff
-        import nexpy.readers.readtxt
-    except ImportError as error:
-        pytest.fail(str(error))
+    """Test that all reader modules can be imported."""
+    import nexpy.gui.pyqt
+    import nexpy.readers.readspec
+    import nexpy.readers.readstack
+    import nexpy.readers.readtiff
+    import nexpy.readers.readtxt
