96f0d9814b5cbbaedb4c66aa32497b0ff9b082c3
Ray Osborn <rayosborn@mac.com>
2026-02-04 09:59:20 -0600

    Simplify the PyQt version numbers

--- src/nexpy/gui/pyqt.py
+++ src/nexpy/gui/pyqt.py
@@ -10,16 +10,16 @@ import os
 from qtpy import QtCore, QtGui, QtWidgets
 
 if QtCore.PYQT5:
-    QtVersion = 'PyQt5' + ' v' + QtCore.__version__
+    QtVersion = f"PyQt5 {QtCore.__version__}"
     os.environ['QT_API'] = 'pyqt5'
 elif QtCore.PYQT6:
-    QtVersion = 'PyQt6' + ' v' + QtCore.__version__
+    QtVersion = f"PyQt6 {QtCore.__version__}"
     os.environ['QT_API'] = 'pyqt6'
 elif QtCore.PYSIDE2:
-    QtVersion = 'PySide2' + ' v' + QtCore.__version__
+    QtVersion = f"PySide2 {QtCore.__version__}"
     os.environ['QT_API'] = 'pyside2'
 elif QtCore.PYSIDE6:
-    QtVersion = 'PySide6' + ' v' + QtCore.__version__
+    QtVersion = f"PySide6 {QtCore.__version__}"
     os.environ['QT_API'] = 'pyside6'
 
 
